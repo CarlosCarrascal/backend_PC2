e4efae97a692bd86802df05d1b2873ef84dea971
Carlos Carrascal <carlos.carrascal@tecsup.edu.pe>
2025-11-03 15:10:50 -0500

    fix db.js

--- config/db.js
+++ config/db.js
@@ -7,8 +7,16 @@ const sequelize = new Sequelize(
   process.env.DB_PASS,
   {
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 18330,
     dialect: "mysql",
     logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      },
+      connectTimeout: 60000 
+    },
     pool: {
       max: 5,
       min: 0,
